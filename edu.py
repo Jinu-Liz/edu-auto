7c61bbe36ebba43a4559f7098b6b306e9c1bf240
hsyoon <bagzein@gmail.com>
2024-06-29 21:50:37 +0900

    - 기존 자동화 로직 리팩터링

--- edu.py
+++ edu.py
@@ -1,13 +1,55 @@
 import time
+import use_chat_gpt as gpt
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 ID = "{아이디}"
 PASSWORD = "{비밀번호}"
+JOKBO_DIC = gpt.get_jokbo_dic()
+
 
 def wait():
     time.sleep(2)
+
+
+def do_test(browser):
+    browser.find_element(By.ID, "exam_agree_check").click()
+    browser.find_element(By.XPATH, '/html/body/div/div/div/div/span[2]/input').click()
+
+    try:
+        browser.switch_to.alert.accept()
+        wait()
+
+    except:
+        pass
+
+    time.sleep(5)
+
+    exam_list = browser.find_element(By.ID, 'exam_list').find_elements(By.CLASS_NAME, 'exam_area')
+    for exam in exam_list:
+        question = exam.find_element(By.CLASS_NAME, 'exam_q').text
+
+        choice_list = exam.find_element(By.CLASS_NAME, 'exam_a').find_elements(By.TAG_NAME, 'a')
+        for choice in choice_list:
+            question = question + choice.text + '\n'
+
+        question = question + '번호로만 대답해 줘'
+        gpt_answer = int(gpt.ask_to_chat_gpt(current_jokbo, question))
+
+        choice_list[gpt_answer - 1].click()
+
+    # / html / body / div / div[2] / div[1] / div[3] / a
+    browser.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[3]/a/span').click()
+    wait()
+
+    browser.find_element(By.ID, 'popup_ok').click()
+    wait()
+
+    browser.find_element(By.ID, 'popup_ok').click()
+    wait()
+
+
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -36,66 +78,95 @@ try:
 except:
     pass
 
+if __name__ == '__main__':
+    try:
+        driver.get("https://www.allteaching.biz/lms/class/student/")
+        wait()
 
-driver.get("https://www.allteaching.biz/lms/class/student/")
-wait()
+        driver.get("https://www.allteaching.biz/lms/class/set_session.php?ps_id=2608cfdf30839b2cd3ebc60fe65ea6ee@kwto@2024&url=%2Flms%2Fclass%2Fstudent%2Fpage.php%3Fp%3Dcl_lecture")
+        wait()
 
-driver.get("https://www.allteaching.biz/lms/class/set_session.php?ps_id=2608cfdf30839b2cd3ebc60fe65ea6ee@kwto@2024&url=%2Flms%2Fclass%2Fstudent%2Fpage.php%3Fp%3Dcl_lecture")
-wait()
+        lecture_table = driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[3]/div/table").find_element(By.TAG_NAME, "tbody")
+        tr_list = lecture_table.find_elements(By.TAG_NAME, "tr")
+        lecture_number = 1
+        for tr in tr_list:
+            td_list = tr.find_elements(By.TAG_NAME, "td")
+            rate_td = td_list[5]
+            listen_td = td_list[7]
+            test_td = td_list[11]
+            current_jokbo = JOKBO_DIC[lecture_number]
+            process_rate = rate_td.find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, "span").text
+            is_listen_completed = process_rate == "100%"
+            is_test_completed = test_td.find_element(By.TAG_NAME, "a").text == "시험완료"
 
-lecture_table = driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[3]/div/table").find_element(By.TAG_NAME, "tbody")
-tr_list = lecture_table.find_elements(By.TAG_NAME, "tr")
-for tr in tr_list:
-    td_list = tr.find_elements(By.TAG_NAME, "td")
-    count = 0
-    for td in td_list:
-        if count == 5:
-            process_rate = td.find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, "span").text
-            if process_rate == "100%":
-                break
+            if is_listen_completed:
+                if is_test_completed:
+                    continue
 
-        if count == 7:
-            td.find_element(By.TAG_NAME, "span").find_element(By.TAG_NAME, "a").click()
-            driver.switch_to.window(driver.window_handles[1])
-            wait()
-
-            try:
-                driver.find_element(By.ID, "agree_chk_safety").click()
-                driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/button[1]").click()
-                wait()
-            except:
-                pass
-
-            try:
-                driver.switch_to.alert.accept()
-                wait()
-            except:
-                pass
-
-            current_page = int(driver.find_element(By.ID, "now").text)
-            total_page = int(driver.find_element(By.ID, "total").text)
-
-            while current_page <= total_page:
-                next_btn = driver.find_element(By.ID, "btn_next")
-                btn_cls_nm = next_btn.get_attribute("class")
-                wait()
-                driver.switch_to.frame(driver.find_element(By.ID, "content_frame"))
-                driver.find_element(By.ID, "volume").click()
-                driver.switch_to.default_content()
-                while btn_cls_nm != "btn_next on":
-                    btn_cls_nm = driver.find_element(By.ID, "btn_next").get_attribute("class")
-                    print(btn_cls_nm)
-                    if "btn_last_page" in btn_cls_nm:
-                        break
-
-                    time.sleep(10)
-
-                if "btn_last_page" in btn_cls_nm:
-                    driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div/button[1]").click()
                 else:
-                    next_btn.click()
-                    current_page = current_page + 1
+                    test_td.find_element(By.TAG_NAME, "span").find_element(By.TAG_NAME, "a").click()
+                    wait()
 
-            driver.switch_to.window(driver.window_handles[0])
+                    do_test(driver)
+            else:
+                try:
+                    listen_td.find_element(By.TAG_NAME, "span").find_element(By.TAG_NAME, "a").click()
+                    driver.switch_to.window(driver.window_handles[1])
+                    wait()
 
-        count = count + 1
+                except:
+                    driver.switch_to.alert.accept()
+                    wait()
+
+                    td_idx = td_idx + 1
+                    continue
+
+                try:
+                    driver.find_element(By.ID, "agree_chk_safety").click()
+                    driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/button[1]").click()
+                    wait()
+                except:
+                    pass
+
+                try:
+                    driver.switch_to.alert.accept()
+                    wait()
+                except:
+                    pass
+
+                current_page = int(driver.find_element(By.ID, "now").text)
+                total_page = int(driver.find_element(By.ID, "total").text)
+
+                while current_page <= total_page:
+                    next_btn = driver.find_element(By.ID, "btn_next")
+                    btn_cls_nm = next_btn.get_attribute("class")
+                    wait()
+                    driver.switch_to.frame(driver.find_element(By.ID, "content_frame"))
+                    driver.find_element(By.ID, "volume").click()
+                    driver.switch_to.default_content()
+                    while btn_cls_nm != "btn_next on":
+                        btn_cls_nm = driver.find_element(By.ID, "btn_next").get_attribute("class")
+                        print(btn_cls_nm)
+                        if "btn_last_page" in btn_cls_nm:
+                            break
+
+                        time.sleep(10)
+
+                    if "btn_last_page" in btn_cls_nm:
+                        try:
+                            driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div/button[1]").click()
+                        except:
+                            driver.close()
+
+                    else:
+                        next_btn.click()
+                        current_page = current_page + 1
+
+                # 회차 끝
+                driver.switch_to.window(driver.window_handles[0])
+
+                do_test(driver)
+
+    except Exception as e:
+        print(e)
+        driver.close()
